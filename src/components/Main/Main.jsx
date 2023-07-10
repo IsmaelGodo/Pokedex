63c8ec7cf364d538967a09c1bd3085021ec5d723
IsmaelGodo <gid4creative@gmail.com>
2023-07-10 22:32:33 +0200

    Final

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './Home/Home';
 import Pokedex from './Home/Pokedex/Pokedex';
 import Lab from './Home/Pokedex/Lab/Lab';
+import Detail from './Home/Pokedex/Detail/Detail';
 
 const Main = () => {
   return (
@@ -11,6 +12,7 @@ const Main = () => {
       <Route path="/" element={<Home/>}/>
       <Route path="/search" element={<Pokedex/>}/>
       <Route path="/lab" element={<Lab/>}/>
+      <Route path="/pokemon/:id" element={<Detail/>}/>
     </Routes>
     {/* <Home dataTrainer={trainer} onDataTrainer={handleTrainer}/> */}
   </main>
